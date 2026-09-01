a974e44a9867a84b002f718869dc4a563aeeab5a
DevsGu <studyguuhss@gmail.comgit config --global user.name DevsGugit config --global user.email studyguuhss@gmail.com>
2026-09-01 14:48:15 -0300

    ✨ feat: Adicionando Get & Set & Contrutor de Produto

--- src/main/java/com/edu/ifce/pecaai/entities/Produto.java
+++ src/main/java/com/edu/ifce/pecaai/entities/Produto.java
@@ -29,4 +29,54 @@ public class Produto {
     @ManyToOne
     @JoinColumn(name = "loja_id", nullable = false)
     private Loja loja;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public Loja getLoja() {
+        return loja;
+    }
+
+    public void setLoja(Loja loja) {
+        this.loja = loja;
+    }
+
+    public Produto(Long id, String nome, String descricao, Double preco, Loja loja) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+        this.loja = loja;
+    }
+
+    
 }
