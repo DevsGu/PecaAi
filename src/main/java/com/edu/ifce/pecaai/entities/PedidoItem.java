80eab118e0f461eaa8c2f69c94ac928d172b8d25
DevsGu <studyguuhss@gmail.comgit config --global user.name DevsGugit config --global user.email studyguuhss@gmail.com>
2026-09-01 14:48:04 -0300

    ✨ feat: Adicionando Get & Set & Contrutor de PedidoItem

--- src/main/java/com/edu/ifce/pecaai/entities/PedidoItem.java
+++ src/main/java/com/edu/ifce/pecaai/entities/PedidoItem.java
@@ -27,4 +27,45 @@ public class PedidoItem {
     @ManyToOne
     @JoinColumn(name = "produto_id", nullable = false)
     private Produto produto;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public PedidoItem(Long id, Integer quantidade, Pedido pedido, Produto produto) {
+        this.id = id;
+        this.quantidade = quantidade;
+        this.pedido = pedido;
+        this.produto = produto;
+    }
+
+    
 }
