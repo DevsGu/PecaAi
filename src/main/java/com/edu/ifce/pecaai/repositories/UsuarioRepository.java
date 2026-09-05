cfba655120320666c3e9e52c046ab7068d777c43
DevsGu <studyguuhss@gmail.comgit config --global user.name DevsGugit config --global user.email studyguuhss@gmail.com>
2026-09-05 16:39:45 -0300

    :recycle: Refactor: Alterando UsuarioRepository

--- src/main/java/com/edu/ifce/pecaai/repositories/UsuarioRepository.java
+++ src/main/java/com/edu/ifce/pecaai/repositories/UsuarioRepository.java
@@ -2,10 +2,12 @@ package com.edu.ifce.pecaai.repositories;
 
 import com.edu.ifce.pecaai.entities.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
-
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    Optional<Usuario> findByEmail(String email);
+    
+    // Método que o Spring Security vai usar para buscar o usuário no login
+    UserDetails findByEmail(String email);
 }
