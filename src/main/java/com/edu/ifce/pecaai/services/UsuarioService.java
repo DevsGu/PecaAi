c08ad5cd86180b80d55c4cbf7ee4ce3636ba4c2e
DevsGu <studyguuhss@gmail.comgit config --global user.name DevsGugit config --global user.email studyguuhss@gmail.com>
2026-09-05 17:29:21 -0300

    :ok_hand: style: Alterações de revisão de código

--- src/main/java/com/edu/ifce/pecaai/services/UsuarioService.java
+++ src/main/java/com/edu/ifce/pecaai/services/UsuarioService.java
@@ -5,6 +5,7 @@ import com.edu.ifce.pecaai.entities.Usuario;
 import com.edu.ifce.pecaai.repositories.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.edu.ifce.pecaai.entities.UserRole;
 
 import java.util.List;
 
@@ -24,13 +25,13 @@ public class UsuarioService {
     }
 
     public Usuario salvar(UsuarioRequestDTO dto) {
-        Usuario usuario = new Usuario();
-        usuario.setNome(dto.nome());
-        usuario.setEmail(dto.email());
-        usuario.setSenha(dto.senha());
-        usuario.setTipo(Usuario.TipoUsuario.valueOf(dto.tipo()));
-        return usuarioRepository.save(usuario);
-    }
+    Usuario usuario = new Usuario();
+    usuario.setNome(dto.nome());
+    usuario.setEmail(dto.email());
+    usuario.setSenha(dto.senha());
+    usuario.setRole(UserRole.valueOf(dto.tipo().toUpperCase()));
+    return usuarioRepository.save(usuario);
+}
 
     public void deletar(Long id) {
         buscarPorId(id);
