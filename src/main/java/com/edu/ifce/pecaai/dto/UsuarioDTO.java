2af353a090053d88970ac14432c165971ce4ddcc
DevsGu <studyguuhss@gmail.comgit config --global user.name DevsGugit config --global user.email studyguuhss@gmail.com>
2026-09-05 16:37:35 -0300

    :recycle: Refactor: Alterações em UsuarioDTO

--- src/main/java/com/edu/ifce/pecaai/dto/UsuarioDTO.java
+++ src/main/java/com/edu/ifce/pecaai/dto/UsuarioDTO.java
@@ -1,15 +1,16 @@
 package com.edu.ifce.pecaai.dto;
 
 import com.edu.ifce.pecaai.entities.Usuario;
+import com.edu.ifce.pecaai.entities.UserRole;
 
 public record UsuarioDTO(Long id, String nome, String email, String senha, String tipo) {
     public static UsuarioDTO fromEntity(Usuario usuario) {
         return new UsuarioDTO(
-            usuario.getId(), 
+            usuario.getId(),
             usuario.getNome(),
             usuario.getEmail(),
-            usuario.getSenha(), 
-            usuario.getTipo() != null ? usuario.getTipo().toString() : null
+            usuario.getSenha(),
+            usuario.getRole() != null ? usuario.getRole().name() : null
         );
     }
 }
